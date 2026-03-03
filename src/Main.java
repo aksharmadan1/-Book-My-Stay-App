28b480d0111f784eb94a0d64b4ac3cc583b56a92
anwesh-23 <anweshdash295@gmail.com>
2026-03-03 15:21:42 +0530

    UC2: Created Room class with static properties

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("******************************************");
-        System.out.println("   WELCOME TO BOOK-MY-STAY HOTEL SYSTEM   ");
-        System.out.println("******************************************");
-        System.out.println("Status: System Initialized...");
+public class Room {
+    private int roomNumber;
+    private String type;
+    private boolean isAvailable;
+
+    public Room(int roomNumber, String type, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.isAvailable = isAvailable;
+    }
+    @Override
+    public String toString() {
+        return "Room " + roomNumber + " [" + type + "] - " + (isAvailable ? "Available" : "Occupied");
     }
 }
